5fd76e1abe85c1073260341c2e8a09c51af36217
Vania Bogdan <bogdan.vania@gmail.com>
2023-08-08 01:14:00 +0300

    steps 1, 2 are done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,58 @@ const express = require('express')
 
 const router = express.Router()
 
+const Contacts = require('../../models/contacts')
+
+const jsonParser = express.json();
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const data = await Contacts.listContacts()
+    res.send(data)
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const contact = await Contacts.getContactById(contactId)
+
+  if (!contact) {
+    res.status(404).json({ message: 'Not found' })
+  }
+
+  res.status(200).send(contact)
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', jsonParser, async (req, res, next) => {
+  const newContact = await Contacts.addContact(req.body)
+  if (!newContact) {
+    res.status(400).json({ message: "missing required name field" })
+  }
+  res.status(201).send(newContact)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const result = await Contacts.removeContact(contactId)
+  if (result === null) {
+    res.status(404).json({ message: 'Not found' })
+  }
+  res.status(200).json({ message: "contact deleted" });
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const updatedContact = await Contacts.updateContact(contactId, req.body)
+  if (updatedContact === undefined) {
+    res.status(404).json({ message: 'Not found' })
+  }
+  if (updatedContact === null) {
+    res.status(400).json({ message: 'missing fields' })
+  }
+  res.status(200).send(updatedContact)
 })
 
 module.exports = router
